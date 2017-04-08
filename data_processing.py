2bd661020fb03aa7bfd14abbd47ec8f26f090f5b
Sourabh Kulahre <sk1846@rit.edu>
2017-04-08 19:52:03 -0400

    Comfirmed inception model

--- data_processing.py
+++ data_processing.py
@@ -2,3 +2,13 @@
 #1-Mean substraction 
 #2-Scaling
 #
+
+def get_train_batch(batch_size):
+
+  return [images,labels]
+
+def get_test_batch(batch_size):
+
+  return [images,labels]
+
+
--- retrain.py
+++ retrain.py
@@ -1,5 +1,5 @@
 # Python script to retrain Google Inception model 
-
+# This code is inspired from this tutorial : https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/image_retraining/retrain.py
 # Imports 
 import os
 import re
@@ -23,7 +23,17 @@ import pickle
 
 
 FLAGS=None
+BOTTLENECK_TENSOR_NAME = 'pool_3/_reshape:0'
+BOTTLENECK_TENSOR_SIZE = 2048
+#MODEL_INPUT_WIDTH = 299
+#MODEL_INPUT_HEIGHT = 299
+MODEL_INPUT_DEPTH = 3
+JPEG_DATA_TENSOR_NAME = 'DecodeJpeg/contents:0'
+RESIZED_INPUT_TENSOR_NAME = 'ResizeBilinear:0'
+MAX_NUM_IMAGES_PER_CLASS = 2 ** 27 - 1  # ~134M
+
 DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+
 def maybe_download_and_extract():
   """Download and extract model tar file."""
   dest_directory = FLAGS.model_dir
@@ -54,23 +64,23 @@ def create_inception_graph():
     with gfile.FastGFile(model_filename, 'rb') as f:
       graph_def = tf.GraphDef()
       graph_def.ParseFromString(f.read())
-#      bottleneck_tensor, jpeg_data_tensor, resized_input_tensor = (
-#          tf.import_graph_def(graph_def, name='', return_elements=[
-#              BOTTLENECK_TENSOR_NAME, JPEG_DATA_TENSOR_NAME,
-#              RESIZED_INPUT_TENSOR_NAME]))
-#  return sess.graph, bottleneck_tensor, jpeg_data_tensor, resized_input_tensor
+      bottleneck_tensor, jpeg_data_tensor, resized_input_tensor = (
+          tf.import_graph_def(graph_def, name='', return_elements=[
+              BOTTLENECK_TENSOR_NAME, JPEG_DATA_TENSOR_NAME,
+              RESIZED_INPUT_TENSOR_NAME]))
+  return sess.graph, bottleneck_tensor, jpeg_data_tensor, resized_input_tensor
 
 def retrain():
   """
   Retrain inception model
   """
-  create_inception_graph()
+  graph,bottleneck_tensor,jpeg_data_tensor,resized_image_tensor = create_inception_graph()
+  
 
 def main(_):
-  #maybe_download_and_extract()
+  maybe_download_and_extract()
   retrain()
-  graph, bottleneck_tensor, jpeg_data_tensor, resized_image_tensor = (
-      create_inception_graph())
+#  graph, bottleneck_tensor, jpeg_data_tensor, resized_image_tensor = (create_inception_graph())
 
 
 if __name__ == '__main__':
@@ -96,6 +106,12 @@ if __name__ == '__main__':
       type=str,
       default='',
       help='Path to test data.'
+  )
+  parser.add_argument(
+      '--summaries_dir',
+      type=str,
+      default='/tmp/retrain_logs',
+      help='Where to save summary logs for TensorBoard.'
   )
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
