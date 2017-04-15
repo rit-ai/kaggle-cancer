8c7198fcd32c7e57102f1a01c853755bb265f6d1
Sourabh Kulahre <sk1846@rit.edu>
2017-04-15 17:42:38 -0400

    Added working inception model with transfer learning

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,2 @@
-#Tasks such as: 
-#1-Mean substraction 
-#2-Scaling
-#
-
-def get_train_batch(batch_size):
-
-  return [images,labels]
-
-def get_test_batch(batch_size):
-
-  return [images,labels]
-
+#work to be done (maybe)
 
